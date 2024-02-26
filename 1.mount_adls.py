83532f39443fa14f6d9a6e5f9a40b62211f1197b
Feiyi-Ding <Feiyi-Ding>
2024-02-26 21:33:52 +0000

    mount adls

---
+++ 1.mount_adls.py
@@ -0,0 +1,50 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Mount Azure Data Lake for the project
+
+# COMMAND ----------
+
+def mount_adls(storage_account_name, container_name):
+    # get secrets from key vault
+    client_id = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-client-id')
+    tenant_id = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-tenant-id')
+    client_secret = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-secret')
+
+    # set Spark configurations
+    configs = {"fs.azure.account.auth.type": "OAuth",
+          "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+          "fs.azure.account.oauth2.client.id": client_id,
+          "fs.azure.account.oauth2.client.secret": client_secret,
+          "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
+    
+    # unmount the cotainer if already exists
+    if any (mount.mountPoint == f"/mnt/{storage_account_name}/{container_name}" for mount in dbutils.fs.mounts()):
+        dbutils.fs.unmount(f"/mnt/{storage_account_name}/{container_name}")
+
+    # mount the storage account container
+    dbutils.fs.mount(
+        source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
+        mount_point = f"/mnt/{storage_account_name}/{container_name}",
+        extra_configs = configs)
+    
+    display(dbutils.fs.mounts())
+
+
+# COMMAND ----------
+
+# Mount the raw container
+mount_adls("project2024formula1dl", "raw")
+
+# COMMAND ----------
+
+#Mount the processed container
+mount_adls("project2024formula1dl", "processed")
+
+# COMMAND ----------
+
+# Mount the presentation container
+mount_adls("project2024formula1dl", "presentation")
+
+# COMMAND ----------
+
+
